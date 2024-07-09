9ebb83bff364af81f9511caa83db9d2ccf1749ec
Lingxuan Ye <love@lingxuan.io>
2024-07-09 09:32:29 +0800

    feat: define trait `ExitOnError<T>` and implement it for `Result<T, E: Into<anyhow::Error>>` and `Option<T>`

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,44 @@
+use anyhow::Error;
+use std::process::exit;
+
 #[macro_use]
 mod macros;
+
+pub trait ExitOnError<T>: internal::Sealed {
+    fn exit_on_error(self) -> T;
+}
+
+impl<T, E> ExitOnError<T> for Result<T, E>
+where
+    E: Into<Error>,
+{
+    fn exit_on_error(self) -> T {
+        match self {
+            Err(error) => {
+                let error = error.into();
+                error!(error);
+                error.chain().skip(1).for_each(|cause| caused_by!(cause));
+                exit(1);
+            }
+            Ok(value) => value,
+        }
+    }
+}
+
+impl<T> ExitOnError<T> for Option<T> {
+    fn exit_on_error(self) -> T {
+        match self {
+            None => {
+                error!("unexpected None");
+                exit(1);
+            }
+            Some(value) => value,
+        }
+    }
+}
+
+mod internal {
+    pub trait Sealed {}
+    impl<T, E> Sealed for Result<T, E> where E: Into<super::Error> {}
+    impl<T> Sealed for Option<T> {}
+}
