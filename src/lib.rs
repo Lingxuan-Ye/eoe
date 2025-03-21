d48c6a42d79a73a552f7e16f2f69992701535a83
Lingxuan Ye <love@lingxuan.io>
2025-03-21 15:26:05 +0800

    feat: allow customization of error styling and messages

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,18 @@
 //! gracefully, leveraging [`anyhow`] to display detailed error
 //! context and chained messages.
 
-use std::process;
+use anyhow::Error;
+use owo_colors::{OwoColorize, Stream, Style};
+use std::fmt::Display;
+use std::io::{StderrLock, Write, stderr};
+use std::process::exit;
+use std::sync::OnceLock;
 
-#[macro_use]
-mod macros;
+pub static ERROR: OnceLock<Segment<&str>> = OnceLock::new();
+pub static CAUSED_BY: OnceLock<Segment<&str>> = OnceLock::new();
+pub static SEP: OnceLock<Segment<&str>> = OnceLock::new();
+pub static MESSAGE_STYLE: OnceLock<Style> = OnceLock::new();
+pub static MESSAGE_ON_NONE: OnceLock<&str> = OnceLock::new();
 
 /// Exits the process with an error message if the result is an error
 /// or the option is `None`.
@@ -37,7 +45,7 @@ pub trait ExitOnError<T>: internal::Sealed {
 
 impl<T, E> ExitOnError<T> for Result<T, E>
 where
-    E: Into<anyhow::Error>,
+    E: Into<Error>,
 {
     /// Exits the process with an error message if the result is an error.
     ///
@@ -55,10 +63,13 @@ where
     fn exit_on_error(self) -> T {
         match self {
             Err(error) => {
-                let error = error.into();
-                error!(error);
-                error.chain().skip(1).for_each(|cause| caused_by!(cause));
-                process::exit(1);
+                let error: Error = error.into();
+                let mut stderr = stderr().lock();
+                print_error(&mut stderr, &error);
+                error.chain().skip(1).for_each(|cause| {
+                    print_caused_by(&mut stderr, cause);
+                });
+                exit(1);
             }
             Ok(value) => value,
         }
@@ -78,8 +89,10 @@ impl<T> ExitOnError<T> for Option<T> {
     fn exit_on_error(self) -> T {
         match self {
             None => {
-                error!("unexpected None");
-                process::exit(1);
+                let mut stderr = stderr().lock();
+                let message = MESSAGE_ON_NONE.get_or_init(|| Fallback::MESSAGE_ON_NONE);
+                print_error(&mut stderr, message);
+                exit(1);
             }
             Some(value) => value,
         }
@@ -117,7 +130,7 @@ pub trait QuitOnError<T>: internal::Sealed {
 
 impl<T, E> QuitOnError<T> for Result<T, E>
 where
-    E: Into<anyhow::Error>,
+    E: Into<Error>,
 {
     /// Quits the process with an error message if the result is an error.
     ///
@@ -151,8 +164,81 @@ impl<T> QuitOnError<T> for Option<T> {
     }
 }
 
+#[derive(Debug)]
+pub struct Segment<T> {
+    pub style: Style,
+    pub value: T,
+}
+
+impl<T> Segment<T>
+where
+    T: Display,
+{
+    fn display(&self, stream: Stream) -> impl Display {
+        self.value
+            .if_supports_color(stream, |value| value.style(self.style))
+    }
+}
+
+struct Fallback;
+
+impl Fallback {
+    const ERROR: Segment<&str> = Segment {
+        style: Style::new().red().bold(),
+        value: "error",
+    };
+    const CAUSED_BY: Segment<&str> = Segment {
+        style: Style::new().red().bold(),
+        value: "caused by",
+    };
+    const SEP: Segment<&str> = Segment {
+        style: Style::new(),
+        value: ": ",
+    };
+    const MESSAGE_STYLE: Style = Style::new();
+    const MESSAGE_ON_NONE: &str = "unexpected None";
+}
+
+fn print_error<M>(stderr: &mut StderrLock, message: M)
+where
+    M: Display,
+{
+    let label = ERROR.get_or_init(|| Fallback::ERROR);
+    print(stderr, label, message);
+}
+
+fn print_caused_by<M>(stderr: &mut StderrLock, message: M)
+where
+    M: Display,
+{
+    let label = CAUSED_BY.get_or_init(|| Fallback::CAUSED_BY);
+    print(stderr, label, message);
+}
+
+fn print<M>(stderr: &mut StderrLock, label: &Segment<&str>, message: M)
+where
+    M: Display,
+{
+    let sep = SEP.get_or_init(|| Fallback::SEP);
+    let message = Segment {
+        style: *MESSAGE_STYLE.get_or_init(|| Fallback::MESSAGE_STYLE),
+        value: message,
+    };
+    if let Err(io_error) = writeln!(
+        stderr,
+        "{}{}{}",
+        label.display(Stream::Stderr),
+        sep.display(Stream::Stderr),
+        message.display(Stream::Stderr)
+    ) {
+        panic!("failed printing to stderr: {}", io_error);
+    }
+}
+
 mod internal {
+    use super::Error;
+
     pub trait Sealed {}
-    impl<T, E> Sealed for Result<T, E> where E: Into<anyhow::Error> {}
+    impl<T, E> Sealed for Result<T, E> where E: Into<Error> {}
     impl<T> Sealed for Option<T> {}
 }
