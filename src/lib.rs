354a58c7762ff13da6f4f7a8bac83afc9c972598
Lingxuan Ye <ink@lingxuan.io>
2026-01-17 02:57:41 +0800

    docs: update documentation

--- src/lib.rs
+++ src/lib.rs
@@ -51,11 +51,7 @@
 //!
 //! let _ = eoe::ERROR.set(Segment {
 //!     style: Style::new().bold().blue(),
-//!     value: "Watchin' some good friends screamin'",
-//! });
-//! let _ = eoe::SEP.set(Segment {
-//!     style: Style::new(),
-//!     value: " 😱 ",
+//!     value: "Watchin' some good friends screamin' 😱",
 //! });
 //! let _ = eoe::MESSAGE_STYLE.set(Style::new().italic().yellow());
 //! let _ = eoe::MESSAGE_ON_NONE.set("Let me out");
@@ -66,7 +62,7 @@
 //! <details>
 //! <summary> Show output </summary>
 //! <div style="background-color: #1e1e1e; font-family: monospace; padding: 10px; border-radius: 5px;">
-//!     <span style="color: #3b8eea; font-weight: bold">Watchin' some good friends screamin'</span><span> 😱 </span><span style="color: #e5e510; font-style: italic">Let me out</span><br>
+//!     <span style="color: #3b8eea; font-weight: bold">Watchin' some good friends screamin' 😱</span> <span style="color: #e5e510; font-style: italic">Let me out</span><br>
 //! </div>
 //! </details>
 
@@ -189,7 +185,7 @@ impl<T> QuitOnError<T> for Option<T> {
     }
 }
 
-/// A labeled message segment.
+/// A message segment.
 #[derive(Debug)]
 pub struct Segment<T> {
     /// The segment's style.
